50d1a6728a8cafc6776e3bd2acb1b739ee0f8af6
jain-aniket <me.aniket.jain@gmail.com>
2022-01-29 21:10:56 -0800

    barrel distortion?

--- data_aug/data_aug.py
+++ data_aug/data_aug.py
@@ -2,9 +2,13 @@ import os
 import random
 from PIL import Image, ImageEnhance, ImageOps
 
+from wand.image import Image
+import numpy as np
+import cv2
 
-input_path = "/Users/aniketjain/Desktop/Code/AltoPonix/foliage-dataset/data_aug"
-output_path = "/Users/aniketjain/Desktop/Code/AltoPonix/foliage-dataset/data_aug"
+
+input_path = "./data_aug"
+output_path = "./data_aug"
 
 def contrast(filename, input_path, contrast_factor, name):
 	ImageEnhance.Contrast(Image.open(input_path + "/" + filename)).enhance(random.uniform(contrast_factor[0], contrast_factor[1])).save(filename[:-4]+ "_" + name + ".png")
@@ -32,15 +36,25 @@ def posterize(filename, input_path, bits, name): # Reduce color in an image. Bas
 	# img = ImageOps.posterize(img, bits)
 	# img.save(filename[:-4]+ "_" + name + ".png")
 
+def barrel(filename, input_path, name):
+	with Image(filename=(input_path + "/" + filename)) as img: # sry for double use of `filename`
+		img.virtual_pixel = 'transparent'
+		img.distort('barrel', (0.2, 0.0, 0.0, 1.0))
+		img.save(filename=filename[:-4]+ "_" + name + ".png")
+		# convert to opencv/numpy array format
 
 for filename in os.listdir(input_path):
 	if filename.endswith(".png"):
-		# Increase brightness in range I tested to be acceptable
-		brightness(filename, input_path, [1.2, 2.5], "a") 
-		# Decrease contrast in range I tested to be acceptable
-		contrast(filename, input_path, [0.2, 0.8], "b")
-		# Decrease color in image to certain number of bits per channel. Higher number = less deep fried
-		posterize(filename, input_path, 8, "c")
+		# # Increase brightness in range I tested to be acceptable
+		# brightness(filename, input_path, [1.2, 2.5], "a") 
+		# # Decrease contrast in range I tested to be acceptable
+		# contrast(filename, input_path, [0.2, 0.8], "b")
+		# # Decrease color in image to certain number of bits per channel. Higher number = less deep fried
+		# posterize(filename, input_path, 8, "c")
+		# Barrel distortion, range, in `barrel_factor` param, coming soon
+		barrel(filename, input_path, "d")
+
+
 
 
 
